2c27e362a588a1923c50c816bd126bbd2b75b5b7
DanMarzo <marzogildan@gmail.com>
2022-08-08 20:25:36 -0300

    Function CriarLista() Funcionario é instaciado

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Aula03Colecoes.Models.Enuns;
 
 namespace Aula03Colecoes
 {
@@ -7,7 +8,19 @@ namespace Aula03Colecoes
          static List<Funcionario> lista = new List<Funcionario>();
         static void Main(string[] args)
         {
-          
+            Criarlista();
         }
+        public static void Criarlista()
+        {
+            Funcionario f1 = new Funcionario();
+            f1.Id = 10;
+            f1.Nome = "Neymar";
+            f1.Cpf = "12345678910";
+            f1.DataAdmissao = DateTime.Parse(01/01/2000);
+            f1.Salario = 100.000M;
+            f1.TipoFuncionario = TipoFuncionarioEnum.CLT;
+            lista.Add(f1);
+        }
+
     }
 }
