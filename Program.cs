bdc4a5c0d9ee89b28222a96da8221569dda1fa5b
DanMarzo <marzogildan@gmail.com>
2022-08-08 20:30:37 -0300

    Correção DateTime.Parse('01/01/2000')

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using Aula03Colecoes.Models;
 using Aula03Colecoes.Models.Enuns;
 
 namespace Aula03Colecoes
 {
     class Program
     {
-         static List<Funcionario> lista = new List<Funcionario>();
+        static List<Funcionario> lista = new List<Funcionario>();
         static void Main(string[] args)
         {
             Criarlista();
@@ -16,7 +17,7 @@ namespace Aula03Colecoes
             f1.Id = 10;
             f1.Nome = "Neymar";
             f1.Cpf = "12345678910";
-            f1.DataAdmissao = DateTime.Parse(01/01/2000);
+            f1.DataAdmissao = DateTime.Parse("01/01/2000");
             f1.Salario = 100.000M;
             f1.TipoFuncionario = TipoFuncionarioEnum.CLT;
             lista.Add(f1);
