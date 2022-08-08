863d476ccc6c3833c846286b44cd6003ab3810df
DanMarzo <marzogildan@gmail.com>
2022-08-08 20:17:39 -0300

    create List<Funcionario>

--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ namespace Aula03Colecoes
 {
     class Program
     {
+         static List<Funcionario> lista = new List<Funcionario>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          
         }
     }
 }
