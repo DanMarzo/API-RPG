dacf103ed286dae23c7007504e04940d3357b11a
DanMarzo <marzogildan@gmail.com>
2022-08-22 21:37:26 -0300

    models criados

---
+++ rgpApi/Models/Personagem.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RpgApi.Models
+{
+    public class Personagem
+    {
+        public int Id { get; set; }
+        
+    }
+}
