9b8a462cec5346c3855be9a6a60ed5c995ea376d
DanMarzo <marzogildan@gmail.com>
2022-08-22 21:40:09 -0300

    Propriedades publicas criadas sobre o pesonagem

--- rgpApi/Models/Personagem.cs
+++ rgpApi/Models/Personagem.cs
@@ -8,6 +8,10 @@ namespace RpgApi.Models
     public class Personagem
     {
         public int Id { get; set; }
-        
+        public string Nome { get; set; }
+        public int PontosVidas { get; set; }
+        public int Forca { get; set; }
+        public int Defesa { get; set; }
+        public int Inteligencia { get; set; }
     }
 }
