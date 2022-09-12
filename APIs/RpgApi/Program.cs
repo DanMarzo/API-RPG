3e1dd278cdf32902e02930a351a51e18e924235b
DanMarzo <marzogildan@gmail.com>
2022-09-12 20:19:11 -0300

    Add ConextionString and Usings

--- APIs/RpgApi/Program.cs
+++ APIs/RpgApi/Program.cs
@@ -1,3 +1,7 @@
+using RpgApi.Data; //chamando o NameSpace
+using Microsoft.EntityFrameworkCore; 
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
